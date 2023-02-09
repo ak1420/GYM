9454811afe432ce7a2358463bbb9a9e5356a436f
ak1420 <77228605+ak1420@users.noreply.github.com>
2023-02-10 02:18:48 +0530

    exercise.js

---
+++ exercise.js
@@ -0,0 +1,24 @@
+[
+    {
+      title: 'Breakfast',
+      data: [{ name: 'Cucumber Detox Water (1 glass)', image: require('../assets/profile-male.png'), time: '6:30 AM', cab: 10, prot: 5, fat: 5, cal: 200 },
+      { name: 'Oats Porridge in Skimmed Milk (1 bowl)', image: require('../assets/profile-male.png'), time: '8:00 AM', cab: 10, prot: 5, fat: 5, cal: 200 },
+      ],
+    },
+    {
+      title: 'Luch',
+      data: [
+        { name: 'Skimmed Milk Paneer (100 grams)', image: require('../assets/profile-male.png'), time: '12:00 PM', cab: 10, prot: 5, fat: 5, cal: 200 },
+        { name: 'Mixed Vegetable Salad (1 katori)', image: require('../assets/profile-male.png'), time: '2:00 PM', cab: 10, prot: 5, fat: 5, cal: 200 },
+        { name: 'Dal(1 katori)Gajar Matar Sabzi (1 katori)', image: require('../assets/profile-male.png'), time: '2:10 PM', cab: 10, prot: 5, fat: 5, cal: 200 },
+        { name: 'Cut Fruits (1 cup) Buttermilk (1 glass)', image: require('../assets/profile-male.png'), time: '4:00 PM', cab: 10, prot: 5, fat: 5, cal: 200 },
+        { name: 'Tea with Less Sugar and Milk (1 teacup)', image: require('../assets/profile-male.png'), time: '5:30 PM', cab: 10, prot: 5, fat: 5, cal: 200 },
+      ],
+    }, {
+      title: 'Dinner',
+      data: [
+        { name: 'Mixed Vegetable Salad (1 katori)', image: require('../assets/profile-male.png'), time: '8:50 PM', cab: 10, prot: 5, fat: 5, cal: 200 },
+        { name: 'Dal (1 katori) Lauki Sabzi (1 katori)', image: require('../assets/profile-male.png'), time: '9:00 PM', cab: 10, prot: 5, fat: 5, cal: 200 },
+      ]
+    }
+  ]
